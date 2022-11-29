18e58120f22d34511a68a7e4a14e78cb13081195
Michael Chinye <michaelchinye2018@gmail.com>
2022-11-29 15:27:09 +0100

    completed static implementation

---
+++ components/index.js
@@ -0,0 +1,10 @@
+import HomeHeader from "./HomeHeader";
+import NFTCard from "./NFTCard";
+import FocusedStatusBar from "./FocusedStatusBar";
+import { CircleButton, RectButton } from "./Buttons";
+import DetailsBid from "./DetailsBid";
+import DetailsDesc from "./DetailsDesc";
+import { SubInfo } from "./SubInfo";
+
+
+export {HomeHeader, NFTCard, FocusedStatusBar, CircleButton, DetailsDesc, DetailsBid, RectButton, SubInfo}
